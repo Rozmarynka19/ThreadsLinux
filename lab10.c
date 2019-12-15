107ec7046f028f15b6cabcc371bb684971aed7d0
Rozmarynka19 <58343748+Rozmarynka19@users.noreply.github.com>
2019-12-15 18:18:03 +0100

    Added files via upload from remote Linux.
    Added source code with implemented distribution data to other arrays (before writing a thread code/function). In addition, uploaded pdf file with the exercise and other two pdf files (study aid).

---
+++ lab10.c
@@ -0,0 +1,93 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char *argv[]){
+  if(argc>3){
+    fprintf(stderr, "Too many arguments!\n");
+    return 1;
+  }
+  if(argc==1)
+  {
+    fprintf(stderr,"Too few arguments!\n");
+    return 1;
+  }
+  /*if( arg1-(int)arg1 != 0 ||  arg2-(int)arg2 != 0 )
+  {
+    fprintf(stderr,"Arguments have to be integers!\n");
+    return 1;
+  }*/
+  int noOfData = atoi(argv[1]); //double arg1 = atof(argv[1]); //n
+  int threads = atoi(argv[2]); //double arg2 = atof(argv[2]); //w
+  //printf("%0.1f %0.1f\n",arg1,arg2);
+
+  if(threads>=noOfData)
+  {
+    fprintf(stderr, "Number of threads have to be less than number of data!\n");
+    return 1;
+  }
+
+  float *data = malloc(sizeof(float)*noOfData);
+
+  if(!data)
+  {
+    fprintf(stderr,"Memory allocation failed.");
+    return 1;
+  }
+
+  srand(0);
+  for(int i=0;i<noOfData;i++)
+    data[i]=1000.*rand()/RAND_MAX;
+
+  printf("After drawing the numbers:\n");
+  for(int i=0;i<noOfData;i++)
+    printf("%f\n",data[i]);
+
+  printf("\n\n");
+
+  //distributing data to other arrays
+  /*if(noOfData%threads==0)
+  {
+    printf("Remainder is 0\n");
+    int divide = noOfData/threads;
+    float **dividedArray=malloc(sizeof(float*)*threads);
+
+    for(int i=0;i<threads;i++)
+      dividedArray[i]=malloc(sizeof(float)*divide);
+
+    for(int i=0,k=0;i<noOfData;k++)
+      for(int j=0;j<divide;j++,i++)
+        dividedArray[k][j]=data[i];
+
+    printf("After distributing the numbers:\n");
+    for(int i=0;i<threads;i++)
+      for(int j=0;j<divide;j++)
+        printf("%f\n",dividedArray[i][j]);
+  }
+  else
+  {*/
+    int divide = noOfData/threads;
+    int remainder = noOfData%threads;
+    printf("Remainder is %d\n",remainder);
+    float **dividedArray=malloc(sizeof(float*)*threads);
+
+    for(int i=0;i<threads-1;i++)
+      dividedArray[i]=malloc(sizeof(float)*divide);
+    dividedArray[threads-1]=malloc(sizeof(float)*(divide+remainder));
+
+    for(int i=0,k=0;i<noOfData-divide-remainder;k++)
+      for(int j=0;j<divide;j++,i++)
+        dividedArray[k][j]=data[i];
+    for(int i=noOfData-divide-remainder,j=0;i<noOfData;j++,i++)
+      dividedArray[threads-1][j]=data[i];
+
+    printf("After distributing the numbers:\n");
+    for(int i=0;i<threads-1;i++)
+      for(int j=0;j<divide;j++)
+        printf("%f\n",dividedArray[i][j]);
+    for(int j=0;j<divide+remainder;j++)
+      printf("%f\n",dividedArray[threads-1][j]);
+  //}
+
+
+  return 0;
+}
